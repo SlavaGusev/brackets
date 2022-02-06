ebdc5cbd88e9f2f4402055acbfccdeb4ed621e04
SlavaGusev <guseff.slawa84@gmail.com>
2022-02-06 18:18:18 +0300

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let arr = [];
+	let arrOpen = [];
+	let arrClose = [];
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		arrOpen.push(bracketsConfig[i][0]);
+		if (bracketsConfig[i][0] != bracketsConfig[i][1])
+			arrClose.push(bracketsConfig[i][1]);
+		else
+			arrClose.push('');
+	}
+	for (let i = 0; i < str.length; i++) {
+		if (!arrOpen.includes(str[i]) && !arrClose.includes(str[i])) {
+			return false;
+		}	
+		if (arr.length == 0 && arrClose.includes(str[i]))
+			return false;
+		if (arr.length > 0 && str[i] == arr[arr.length - 1] && arrClose[arrOpen.indexOf(arr[arr.length - 1])] == '') {			
+			arr.pop();	
+			continue;
+		}
+		if (arr.length > 0 && arrOpen.indexOf(arr[arr.length - 1]) == arrClose.indexOf(str[i]))
+		{
+			arr.pop();	
+			continue;
+		}	
+		if (arrOpen.includes(str[i])) {
+			arr.push(str[i]);
+			continue;
+		}	
+		if (arrOpen.indexOf(arr[arr.length - 1]) == arrClose.indexOf(str[i]))
+		{
+			arr.pop();	
+			continue;
+		}
+		if (arr.length > 0 && arrClose.includes(str[i]) && arrClose.indexOf(arr[arr.length - 1]) != arrClose.indexOf(str[i])) {
+			return false;
+		} 				
+	}
+	if (arr.length == 0)
+		return true;
+	else
+		return false;
 }
